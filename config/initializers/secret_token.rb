83660ca2be602676c217f9d735a0c38fd44c33e9
Max Miller <aspmiller007@gmail.com>
2012-02-02 08:51:51 -0300

    Teste de commit com aplicativo github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sistemico::Application.config.secret_token = '7003bf0bced099df36ffe815b7506c91046b4b78dfbdbc1dd36ba550e6b84dd5e93b3f97c132beb12be6da7483f18b0d6ace1fa3bb90574ac48d68a18ca1cd25'
